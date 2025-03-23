17544335f7ed7e7fce43bd0113517c22253ea8ae
Enrico Bellagamba <enri.bellagamba@gmail.com>
2025-03-23 20:31:35 +0100

    creazione file biblioteca

--- biblioteca/src/main/java/com/biblioteca/model/Biblioteca.java
+++ biblioteca/src/main/java/com/biblioteca/model/Biblioteca.java
@@ -1,4 +1,22 @@
 package com.biblioteca.model;
 
+import java.util.Arrays;
+
+class Inventario {
+
+    // Metodo che restituisce un array di libri
+    public String[] elencoLibri() {
+        String elenco[] = {"Quello che so di te", "Fratellino", "Macroeconomia", "Un mondo nuovo", "Il nome della rosa"};
+        return elenco;
+    }
+}
+
 public class Biblioteca {
+    public static void main(String[] args) {
+
+        Inventario biblioteca = new Inventario(); // Creiamo un'istanza di Biblioteca
+        String[] elenco = biblioteca.elencoLibri(); // Recuperiamo l'elenco dei libri
+
+        System.out.println("L'elenco dei libri disponibili: " + Arrays.toString(elenco));
+    }
 }
