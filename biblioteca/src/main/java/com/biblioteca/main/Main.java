34a6ed1f88c8bc6eb977b47fc71632d52f66b2c3
Enrico Bellagamba <enri.bellagamba@gmail.com>
2025-03-27 17:37:23 +0100

    modifica dei file Biblioteca e Main

--- biblioteca/src/main/java/com/biblioteca/main/Main.java
+++ biblioteca/src/main/java/com/biblioteca/main/Main.java
@@ -1,7 +1,15 @@
 package com.biblioteca.main;
 
+import com.biblioteca.model.Biblioteca;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Biblioteca biblioteca = new Biblioteca(); // Creiamo un'istanza di Biblioteca
+        String[] elenco = biblioteca.elencoLibri(); // Recuperiamo l'elenco dei libri
+
+        System.out.println("L'elenco dei libri disponibili: " + Arrays.toString(elenco));
     }
 }
--- biblioteca/src/main/java/com/biblioteca/model/Biblioteca.java
+++ biblioteca/src/main/java/com/biblioteca/model/Biblioteca.java
@@ -2,21 +2,35 @@ package com.biblioteca.model;
 
 import java.util.Arrays;
 
-class Inventario {
+public class Biblioteca {
+    private Libro[] dati = new Libro[100];
+    private int size = 0;
 
-    // Metodo che restituisce un array di libri
+    public void aggiungi(Libro l) {
+        if (size < dati.length) {
+            dati[size++] = l;
+        }
+    }
+
+    public void rimuovi(int index) {
+        if (index >= 0 && index < size) {
+            for (int i = index; i < size - 1; i++) {
+                dati[i] = dati[i + 1];
+            }
+            dati[--size] = null;
+        }
+    }
+
+    public int dimensione() {
+        return size;
+    }
+
+    public Libro get(int index) {
+        return dati[index];
+    }
+
     public String[] elencoLibri() {
         String elenco[] = {"Quello che so di te", "Fratellino", "Macroeconomia", "Un mondo nuovo", "Il nome della rosa"};
         return elenco;
-    }
-}
-
-public class Biblioteca {
-    public static void main(String[] args) {
-
-        Inventario biblioteca = new Inventario(); // Creiamo un'istanza di Biblioteca
-        String[] elenco = biblioteca.elencoLibri(); // Recuperiamo l'elenco dei libri
-
-        System.out.println("L'elenco dei libri disponibili: " + Arrays.toString(elenco));
     }
 }
