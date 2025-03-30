1607c36feb32ae9336a0406a1ed84f2df46bb9ac
gaetano carpentiere <gaetanocarpentiere71@gmail.com>
2025-03-30 18:36:54 +0200

    gCReazione libro

--- biblioteca/src/main/java/com/biblioteca/main/Main.java
+++ biblioteca/src/main/java/com/biblioteca/main/Main.java
@@ -1,7 +1,16 @@
 package com.biblioteca.main;
 
+import com.biblioteca.model.Libro;
+
+
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Libro libro1 =new Libro("Quello che so di te", "Nadia Terranova", 2025,  9788823521234L);
+        Libro libro2 = new Libro("Fratellino" ,"di Ibrahima Balde e Amets",2025,  9788807895678L);
+        Libro libro3 = new Libro("Macroeconomia","N. Gregory Manki",2016, 9788880085096L);
+        Libro libro4 = new Libro("Il nome della rosa", " Umberto Eco", 1980,  9788845240000L);
+        Libro libro5 = new Libro( "Un mondo nuovo", "Liz Braswell" , 2015, 9781788107686L);
+
     }
 }
--- biblioteca/src/main/java/com/biblioteca/model/Libro.java
+++ biblioteca/src/main/java/com/biblioteca/model/Libro.java
@@ -2,14 +2,14 @@ package com.biblioteca.model;
 
 
 // Classe che rappresenta un libro nella biblioteca
-class Libri {
+class Libro {
     private String titolo;
     private String autore;
     private int annoPubblicazione;
     private long ISBN;
 
     // Costruttore di default
-    public Libri() {
+    public Libro() {
         titolo= "titolo";
         autore= "sconoscuto";
         annoPubblicazione= 0000;
@@ -17,7 +17,7 @@ class Libri {
     }
 
     // Costruttore parametrizzato
-    public Libri(String titolo, String autore, int annoPubblicazione, long  ISBN) {
+    public Libro(String titolo, String autore, int annoPubblicazione, long  ISBN) {
         this.titolo = titolo;
         this.autore = autore;
         this.annoPubblicazione = annoPubblicazione;
@@ -66,20 +66,5 @@ class Libri {
                 ", Anno di Pubblicazione=" + annoPubblicazione +
                 ", ISBN='" + ISBN + '\'' +
                 '}';
-    }
-}
-public class Libro {
-    public static void main(String[] args) {
-        Libri libro1 =new Libri("Quello che so di te", "Nadia Terranova", 2025,  9788823521234L);
-        Libri libro2 = new Libri("Fratellino" ,"di Ibrahima Balde e Amets",2025,  9788807895678L);
-        Libri libro3 = new Libri("Macroeconomia","N. Gregory Manki",2016, 9788880085096L);
-        Libri libro4 = new Libri("Il nome della rosa", " Umberto Eco", 1980,  9788845240000L);
-        Libri libro5 = new Libri( "Un mondo nuovo", "Liz Braswell" , 2015, 9781788107686L);
-//stampa i libri
-        System.out.println(libro1);
-        System.out.println(libro2);
-        System.out.println(libro3);
-        System.out.println(libro4);
-        System.out.println(libro5);
     }
 }
